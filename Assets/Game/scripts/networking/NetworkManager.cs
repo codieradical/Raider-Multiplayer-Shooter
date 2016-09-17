861f2d4eb3489f61d17e25bdb22c5df783503971
Alex-231 <git+a13x231mcsl@gmal.com>
2016-09-17 23:47:23 +0100

    Lobby Networking scaffolding (sync).

---
+++ Assets/Game/scripts/networking/NetworkManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Raider.Game.Networking
+{
+
+    public class NetworkManager : UnityEngine.Networking.NetworkLobbyManager
+    {
+        #region singleton setup
+
+        //This class already inherits a singleton...
+        NetworkManager instance;
+
+        void Awake()
+        {
+            if (instance != null)
+                Debug.LogError("More than one NetworkManager are active! What are you doing!!");
+            instance = this;
+        }
+
+        void OnDestroy()
+        {
+            Debug.LogError("Something just destroyed the NetworkManager!");
+            instance = null;
+        }
+
+        #endregion
+
+
+        GameObject lobby;
+        List<PlayerData> players
+        {
+            get
+            {
+                List<PlayerData> _players = new List<PlayerData>();
+                foreach(Transform playerObject in lobby.transform)
+                {
+                    _players.Add(playerObject.GetComponent<PlayerData>());
+                }
+                return _players;
+            }
+        }
+
+
+        // Use this for initialization
+        void Start()
+        {
+            lobby = new GameObject("_Lobby");
+            DontDestroyOnLoad(lobby);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public new void StartClient()
+        {
+            base.StartClient();
+        }
+
+        public new void StartHost()
+        {
+            base.StartHost();
+        }
+    }
+}
---
+++ Assets/Game/scripts/networking/PlayerData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Raider.Game.Saves;
+using Raider.Game.Scene;
+
+public class PlayerData : MonoBehaviour {
+
+    public string username;
+    public SaveDataStructure.Character character;
+    public Scenario currentScenario;
+
+}
