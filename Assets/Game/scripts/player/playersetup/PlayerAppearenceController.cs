7180d4df49e5901d4a1aef431405bce58d03c5cf
Alex <A13x231mcsl@gmail.com>
2016-12-27 20:20:22 +0000

    Missed from last commit >_<

--- Assets/Game/scripts/player/playersetup/PlayerAppearenceController.cs
+++ Assets/Game/scripts/player/playersetup/PlayerAppearenceController.cs
@@ -2,118 +2,44 @@
 using Raider.Game.Saves;
 using System.Collections;
 using Raider.Game.GUI.Components;
+using Raider.Game.GUI.CharacterPreviews;
 
 namespace Raider.Game.Player
 {
-    public class PlayerAppearenceController : MonoBehaviour
+    public class PlayerAppearenceController : CharacterPreviewAppearenceController
     {
-        [System.Serializable]
-        public class RaceGraphics
-        {
-            public GameObject xRaceGraphics;
-            public GameObject yRaceGraphics;
-            public Avatar xRaceAvatar;
-            public Avatar yRaceAvatar;
-
-            public void CheckAllGraphicsPresent()
-            {
-                if (xRaceGraphics == null || yRaceGraphics == null || xRaceAvatar == null || yRaceAvatar == null)
-                    Debug.LogError("The player is missing a model prefab or avatar!!!");
-            }
-
-            public GameObject GetGraphicsByRace(SaveDataStructure.Character.Race race)
-            {
-                if (race == SaveDataStructure.Character.Race.X)
-                    return xRaceGraphics;
-                else if (race == SaveDataStructure.Character.Race.Y)
-                    return yRaceGraphics;
-                else
-                {
-                    Debug.LogError("Couldn't find graphics for race " + race.ToString());
-                    return null;
-                }
-            }
-
-            public Avatar GetAvatarByRace(SaveDataStructure.Character.Race race)
-            {
-                if (race == SaveDataStructure.Character.Race.X)
-                    return xRaceAvatar;
-                else if (race == SaveDataStructure.Character.Race.Y)
-                    return yRaceAvatar;
-                else
-                {
-                    Debug.LogError("Couldn't find avatar for race " + race.ToString());
-                    return null;
-                }
-            }
-        }
-        [SerializeField] //For editing in the inspector.
-        private RaceGraphics editorRaceGraphics;
-        public static RaceGraphics GetRaceGraphics { get; private set; }
-
-        public EmblemHandler emblem;
-
-        public GameObject primaryMesh;
-        public GameObject secondaryMesh;
-        public GameObject tertiaryMesh;
-
-        //Nodes used for armour in the future.
-        public GameObject helmetNode;
-        public GameObject shoulderNode;
-        public GameObject bodyNode;
-
-        Renderer primaryRenderer;
-        Renderer secondaryRenderer;
-        Renderer tertiaryRenderer;
-
         private void Awake()
         {
-            editorRaceGraphics.CheckAllGraphicsPresent();
-            GetRaceGraphics = editorRaceGraphics;
+            PlayerResourceReferences.instance.raceGraphics.CheckAllGraphicsPresent();
         }
 
-        // Use this for initialization
-        void Start()
-        {   
-            if (primaryMesh == null || secondaryMesh == null /*|| tertiaryMesh == null*/)
-                Debug.LogError("[Player/PlayerAppearenceController] Player appearence controller is missing a mesh.");
-
-            primaryRenderer = primaryMesh.GetComponent<Renderer>();
-            secondaryRenderer = secondaryMesh.GetComponent<Renderer>();
-            //tertiaryRenderer = tertiaryMesh.GetComponent<Renderer>();
-        }
-
-        public void UpdatePlayerAppearence(SaveDataStructure.Character _character)
+        /// <summary>
+        /// Creates a new graphics model, and deletes the existing instance.
+        /// Returns the new PlayerAppearenceController.
+        /// </summary>
+        /// <param name="playerData">The appearence data for the graphics model.</param>
+        public void ReplaceGraphicsModel(PlayerData playerData)
         {
-            //If primary renderer is null, try to get it again, failing that, return.
-            if (primaryRenderer == null)
-            {
-                Start();
-                if (primaryRenderer == null)
-                    return;
-            }
-            primaryRenderer.material.color = _character.armourPrimaryColor.Color;
-            secondaryRenderer.material.color = _character.armourSecondaryColor.Color;
-            //tertiaryRenderer.material.color = _character.armourTertiaryColor.color;
-
-            emblem.UpdateEmblem(_character);
-        }
-
-        public static void SetupGraphicsModel(PlayerData playerData)
-        {
+            //When switching graphics, the animator freezes.
+            //Nulling the animator, then assigning after graphics setup fixes this.
+            playerData.animator.avatar = null;
             playerData.animator.enabled = false;
 
             //Spawn the graphics.
-            playerData.graphicsObject = Instantiate(GetRaceGraphics.GetGraphicsByRace(playerData.character.race)) as GameObject;
+            playerData.graphicsObject = Instantiate(PlayerResourceReferences.instance.raceGraphics.GetGraphicsByRace(playerData.character.race)) as GameObject;
             playerData.graphicsObject.transform.SetParent(playerData.gameObject.transform, false);
-
-            //Setup the animator
-            playerData.animator.avatar = GetRaceGraphics.GetAvatarByRace(playerData.character.race);
+            playerData.graphicsObject.name = "Graphics"; //Prevents infinate (clone) appends.
 
             //Update the colors, emblem.
-            playerData.graphicsObject.GetComponent<PlayerAppearenceController>().UpdatePlayerAppearence(playerData.character);
+            PlayerAppearenceController newAppearenceController = playerData.graphicsObject.GetComponent<PlayerAppearenceController>();
+            newAppearenceController.UpdatePlayerAppearence(playerData.character);
 
+            //Setup the animator
             playerData.animator.enabled = true;
+            playerData.animator.avatar = PlayerResourceReferences.instance.raceGraphics.GetAvatarByRace(playerData.character.race);
+
+            //This old appearence controller and graphics object is no longer needed.
+            Destroy(gameObject);
         }
     }
 }
