c830c104be25c702318160e25b33433c3214c789
Alex <A13x231mcsl@gmail.com>
2017-01-12 16:47:08 +0000

    Character Selection Reload Hook (sync)

--- Assets/Game/scripts/gui/Mainmenu/Character Selection/CharacterSelectionHandler.cs
+++ Assets/Game/scripts/gui/Mainmenu/Character Selection/CharacterSelectionHandler.cs
@@ -19,6 +19,19 @@ namespace Raider.Game.GUI.Screens
         public const string PREVIEW_CHARACTER_NAME = "plate";
         const CharacterPreviewHandler.PreviewType PREVIEW_TYPE = CharacterPreviewHandler.PreviewType.Plate;
 
+        public void Start()
+        {
+            Session.AddReloadHook(UserReloadedHook);
+        }
+
+        public void UserReloadedHook()
+        {
+            if(MenuManager.instance.CurrentMenu == GetComponent<Menu>())
+            {
+                LoadCharacterPlates();
+            }
+        }
+
         public void LoadCharacterPlates()
         {
             bool platesPreviouslyExisted = false;
